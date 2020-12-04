a3d5a321849943d882fa524ae92eb7257e4d00cd
toilaloc <locpc.qb@gmail.com>
2020-12-04 20:29:13 +0700

    add resolre reporting

--- resources/views/admin/reports/index.blade.php
+++ resources/views/admin/reports/index.blade.php
@@ -17,7 +17,7 @@
         <th scope="col">Người bị báo cáo</th>
         <th scope="col">Loại báo cáo</th>
         <th scope="col">Xem</th>
-        {{-- <th scope="col">Xử lý</th> --}}
+        <th scope="col">Xử lý</th>
       </tr>
     </thead>
     <tbody>
@@ -25,7 +25,11 @@
        
         <tr>
         <th>{{$report->id}}</th>
-        <td> @if($report->post_id != NULL){{$report->Post->name}} @endif</td>
+        <td> 
+          @if($report->post_id != NULL && $report->type == "post"){{"Bài viết"}} <a href="{{$report->Post->name}}">{{$report->Post->name}}</a> @endif
+          @if($report->user_id != NULL && $report->type == "user"){{"Người dùng"}} <a href="{{url('author',$report->User->username)}}">{{$report->User->fullname}}</a> @endif
+          @if($report->comment_id != NULL && $report->type == "comment"){{"Bình luận"}} "{{$report->reportComment->content}}" @endif     
+        </td>
         <th>{{$report->reason}}</th>
         <td>{{$report->Reporter->fullname}}</td>
         <td> @if($report->post_id != NULL){{App\Models\Users::find($report->Post->author_id)->fullname}} @endif</td>
@@ -33,13 +37,37 @@
         <td>
         <a type="submit" class="btn btn-primary btn-sm d-inline" href="@if($report->post_id != NULL){{url('post', $report->Post->slug)}} @endif">Xem</a>
         </td>
-        {{-- <td>
-            <form class="d-inline" action="" method="POST">
+        <td>
+            @if($report->post_id != NULL && $report->type == "post")
+        <form class="d-inline" action="{{route('posts.destroy', $report->post_id)}}" method="POST">
                 @csrf
                 @method('DELETE')
-                <button type="submit" class="btn btn-danger btn-sm d-inline" onclick="return confirm('bạn có chắc muốn xóa');">Xử lý</button>
+                <button type="submit" class="btn btn-danger btn-sm d-inline" onclick="return confirm('Bạn sẽ xóa bài viết này. Bạn chắc chắn chứ?');">Xử lý</button>
+        </form>
+            @endif
+
+            {{-- Nếu là comment --}}
+            @if($report->comment_id != NULL && $report->type == "comment")
+        <form class="d-inline" action="{{route('comments.destroy', $report->comment_id)}}" method="POST">
+                @csrf
+                @method('DELETE')
+                <button type="submit" class="btn btn-danger btn-sm d-inline" onclick="return confirm('Bạn sẽ xóa bình luận này. Bạn chắc chắn chứ?');">Xử lý</button>
+        </form>
+            @endif
+
+            {{-- Nếu là người dùng thì đếm số lần vi phạm. Nếu quá 3 lần sẽ khóa tài khoản --}}
+            @if($report->user_id != NULL && $report->type == "user")
+            @if($report->where('user_id', $report->user_id)->where('type', 'user')->get()->count() >= 3)
+      <form class="d-inline" action="" method="POST">
+              @csrf
+              @method('DELETE')
+              <button type="submit" class="btn btn-danger btn-sm d-inline" onclick="return confirm('Bạn sẽ khóa tài khoản này. Bạn chắc chắn chứ?');">Khóa tài khoản</button>
             </form>
-            </td> --}}
+            @else
+          <button type="submit" class="btn btn-danger btn-sm d-inline" >{{$report->where('user_id', $report->user_id)->where('type', 'user')->get()->count()}} {{"lần bị báo cáo  "}}</button>
+            @endif
+            @endif
+            </td>
         </tr>
        
           @endforeach
